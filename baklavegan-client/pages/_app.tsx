818fdc174e8a86c683b319e2637fe7d6f9f2e108
Kanui <marley.kanui@gmail.com>
2020-12-21 15:47:19 -0600

    baklavegan - global commit 04 - Added threejs and react-three-fiber functionality

--- baklavegan-client/pages/_app.tsx
+++ baklavegan-client/pages/_app.tsx
@@ -1,6 +1,9 @@
 // Next Types
 import type { AppProps } from 'next/app';
 
+// Global Styles
+import '../styles/globals.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
---
+++ baklavegan-client/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BaklaHome = () => {
+  return <h1>Baklavegan</h1>;
+};
+
+export default BaklaHome;
