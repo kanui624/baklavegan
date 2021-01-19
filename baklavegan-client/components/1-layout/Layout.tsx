5e46c26c53a6416850e441f32805a8829164332c
Kanui <marley.kanui@gmail.com>
2021-01-19 13:27:08 -0600

    baklavegan - global commit 74 - moved enter button to layout component

--- baklavegan-client/components/1-layout/Layout.tsx
+++ baklavegan-client/components/1-layout/Layout.tsx
@@ -1,6 +1,13 @@
 // React
 import { useState, Fragment, useEffect } from 'react';
 
+// Next
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+// React Spring
+import { useSpring, animated } from 'react-spring';
+
 // GSAP
 import { gsap } from 'gsap';
 
@@ -19,7 +26,9 @@ interface LayoutProps {
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
+  const router = useRouter();
   const [clicked, setClicked] = useState(false);
+  const [menuClicked, setMenuClicked] = useState(false);
   const [ready, setReady] = useState(false);
   const [disabled, setDisabled] = useState(false);
 
@@ -39,24 +48,13 @@ const Layout: FC<LayoutProps> = ({ children }) => {
     gsap.to('#canvas', { display: 'none', delay: 1.5 });
   };
 
-  const buttonTl = gsap.timeline();
-
   const initialButtonLoad = () => {
-    buttonTl.to('#menubutton', { display: 'block', delay: 2 });
-    buttonTl.to('#menubutton', { opacity: 1, duration: 0.5 });
+    gsap.to('#menubutton', { opacity: 1, duration: 4, delay: 2 });
   };
 
-  useEffect(() => {
-    initialButtonLoad();
-  }, []);
-
-  useEffect(() => {
-    if (ready && clicked) {
-      animateMenuIn();
-    } else {
-      animateMenuOut();
-    }
-  }, [clicked, ready]);
+  const { y } = useSpring({
+    y: menuClicked ? '20rem' : '0rem',
+  });
 
   const toggleClick = () => {
     if (!disabled) {
@@ -68,11 +66,56 @@ const Layout: FC<LayoutProps> = ({ children }) => {
     }
   };
 
+  const handleMenuClick = () => {
+    setMenuClicked(true);
+    setTimeout(() => {
+      toggleClick();
+    }, 1.2);
+  };
+
+  useEffect(() => {
+    initialButtonLoad();
+    console.log(router.pathname);
+  }, []);
+
+  useEffect(() => {
+    if (ready && clicked) {
+      animateMenuIn();
+    } else {
+      animateMenuOut();
+    }
+  }, [clicked, ready]);
+
   return (
     <Fragment>
       <div className="fixed inset-0 h-full max-w-full">
         <div className="flex justify-center items-center container mx-auto h-full">
-          {children}
+          <div className="absolute">{children}</div>
+          <button
+            id="menubutton"
+            className={`${styles.enterbtn} flex justify-center items-center opacity-0`}
+            onClick={() => {
+              handleMenuClick();
+            }}
+          >
+            <animated.div
+              className={`${styles.enterimage} absolute bottom-24`}
+              style={{ transform: y.to((y) => `translateY(${y})`) }}
+            >
+              <Image
+                src="/2-images/1-index/1-enter-btn.png"
+                alt="enter"
+                width={175}
+                height={219}
+              />
+            </animated.div>
+            <animated.div
+              className={`${styles.entertext} absolute bottom-40`}
+              style={{ transform: y.to((y) => `translateY(${y})`) }}
+            >
+              <h3>enter</h3>
+            </animated.div>
+          </button>
         </div>
       </div>
       <div
@@ -86,16 +129,19 @@ const Layout: FC<LayoutProps> = ({ children }) => {
           onCompile={() => setReady(true)}
         />
       </div>
-      <button
-        id="menubutton"
-        className={`${styles.menubuttonload} fixed`}
-        onClick={() => toggleClick()}
-        disabled={disabled}
-      >
-        open
-      </button>
     </Fragment>
   );
 };
 
 export default Layout;
+
+{
+  /* <button
+  id="menubutton"
+  className={`${styles.menubuttonload} fixed`}
+  onClick={() => toggleClick()}
+  disabled={disabled}
+>
+  open
+</button>; */
+}
--- baklavegan-client/pages/index.tsx
+++ baklavegan-client/pages/index.tsx
@@ -1,11 +1,8 @@
+// React
+import { useEffect, Fragment } from 'react';
+
 // Next
 import Image from 'next/image';
-
-// React
-import { useState, useEffect, Fragment } from 'react';
-
-// React Spring
-import { useSpring, animated } from 'react-spring';
 
 // GSAP
 import { gsap } from 'gsap';
@@ -17,67 +14,23 @@ import styles from '../styles/Pages/index.module.scss';
 import { FC } from 'react';
 
 const BaklaHome: FC = () => {
-  const [enter, setEnter] = useState(false);
-  const [pointerOver, setPointerOver] = useState(false);
-
-  const animateIn = () => {
-    gsap.to('.animatelogo', { opacity: 1, duration: 4, delay: 1 });
-    gsap.to('.animatebtn', { opacity: 1, duration: 4, delay: 2 });
+  const animateLogoIn = () => {
+    gsap.to('#animatelogo', { opacity: 1, duration: 4, delay: 1 });
   };
-
-  const handlePointerOver = (over: boolean) => {
-    if (over) {
-      setPointerOver(true);
-      document.body.style.cursor = 'pointer';
-    } else {
-      setPointerOver(false);
-      document.body.style.cursor = 'default';
-    }
-  };
-
-  const { y } = useSpring({ y: enter ? '20rem' : '0rem' });
 
   useEffect(() => {
-    animateIn();
+    animateLogoIn();
   }, []);
 
   return (
-    <Fragment>
-      <div className="absolute">
-        <Image
-          className={`${styles.bvlogo} animatelogo`}
-          src="/2-images/1-index/0-bv-logo.png"
-          alt="baklavegan"
-          width={1400}
-          height={343}
-        />
-      </div>
-      <button
-        className={`${styles.enterbtn} flex justify-center items-center opacity-0 animatebtn`}
-        onPointerOver={() => handlePointerOver(true)}
-        onClick={() => {
-          setEnter(true);
-        }}
-      >
-        <animated.div
-          className={`${styles.enterimage} absolute bottom-24`}
-          style={{ transform: y.to((y) => `translateY(${y})`) }}
-        >
-          <Image
-            src="/2-images/1-index/1-enter-btn.png"
-            alt="enter"
-            width={175}
-            height={219}
-          />
-        </animated.div>
-        <animated.div
-          className={`${styles.entertext} absolute bottom-40`}
-          style={{ transform: y.to((y) => `translateY(${y})`) }}
-        >
-          <h3>enter</h3>
-        </animated.div>
-      </button>
-    </Fragment>
+    <Image
+      id="animatelogo"
+      className={styles.bvlogo}
+      src="/2-images/1-index/0-bv-logo.png"
+      alt="baklavegan"
+      width={1400}
+      height={343}
+    />
   );
 };
 
