1c8759cbbd948390156baa4849a6b8ddda7f3dd1
Kanui <marley.kanui@gmail.com>
2021-01-20 23:19:57 -0600

    baklavegan - global commit 77 - Moved Social Data to clean up contact component and added type interface for social data props

---
+++ baklavegan-client/components/6-contact/SocialData.tsx
@@ -0,0 +1,30 @@
+export const socialData: any = [
+  {
+    id: 0,
+    link: 'https://www.instagram.com/baklavegan/',
+    image: 'instagram',
+    width: 80,
+    height: 80,
+  },
+  {
+    id: 1,
+    link: 'https://www.youtube.com/',
+    image: 'youtube',
+    width: 114,
+    height: 80,
+  },
+  {
+    id: 2,
+    link: 'https://twitter.com/',
+    image: 'twitter',
+    width: 80,
+    height: 80,
+  },
+  {
+    id: 3,
+    link: 'https://www.facebook.com/',
+    image: 'facebook',
+    width: 80,
+    height: 80,
+  },
+];
--- baklavegan-client/pages/baklavegan/contact.tsx
+++ baklavegan-client/pages/baklavegan/contact.tsx
@@ -2,58 +2,42 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+// Data
+import { socialData } from '../../components/6-contact/SocialData';
+
 // Styles
 import styles from '../../styles/Pages/contact.module.scss';
 
 // React Types
 import { FC } from 'react';
 
-const contact = () => {
-  const socials = [
-    {
-      id: 0,
-      link: 'https://www.instagram.com/baklavegan/',
-      image: 'instagram',
-      width: 80,
-      height: 80,
-    },
-    {
-      id: 1,
-      link: 'https://www.youtube.com/',
-      image: 'youtube',
-      width: 114,
-      height: 80,
-    },
-    {
-      id: 2,
-      link: 'https://twitter.com/',
-      image: 'twitter',
-      width: 80,
-      height: 80,
-    },
-    {
-      id: 3,
-      link: 'https://www.facebook.com/',
-      image: 'facebook',
-      width: 80,
-      height: 80,
-    },
-  ];
+// Component level Types
+interface SocialDataProps {
+  id: number;
+  link: string;
+  image: string;
+  width: number;
+  height: number;
+}
+
+const contact: FC = () => {
   return (
     <div className="absolute bottom-24 -inset-x-0 container mx-auto">
       <div className="flex flex-row justify-evenly">
-        {socials.map(({ id, link, image, width, height }) => (
-          <Link key={id} href={link}>
-            <a>
-              <Image
-                src={`/2-images/5-contact/${image}.png`}
-                alt={image}
-                width={width}
-                height={height}
-              />
-            </a>
-          </Link>
-        ))}
+        {socialData.map(
+          ({ id, link, image, width, height }: SocialDataProps) => (
+            <Link key={id} href={link}>
+              <a>
+                <Image
+                  src={`/2-images/5-contact/${image}.png`}
+                  alt={image}
+                  width={width}
+                  height={height}
+                />
+              </a>
+            </Link>
+          )
+        )}
       </div>
     </div>
   );
