e8593f0421d95e7293c9085afbad0b5dce089676
Kanui <marley.kanui@gmail.com>
2021-02-25 23:29:34 -0600

    baklavegan - global commit 159 - Tweaked devnote animation

--- baklavegan-client/animations/1-layout/DevNoteAnimations.tsx
+++ baklavegan-client/animations/1-layout/DevNoteAnimations.tsx
@@ -4,5 +4,10 @@ import gsap from 'gsap';
 export const showDevNote = (devPage: string) => {
   const showDevNoteTl = gsap.timeline();
   showDevNoteTl.to(devPage, { y: -200, duration: 1.5, ease: 'back.out(1.02)' });
-  showDevNoteTl.to(devPage, { y: 100, delay: 1.7, ease: 'back.in(.75)' });
+  showDevNoteTl.to(devPage, {
+    y: 100,
+    delay: 1.7,
+    duration: 0.8,
+    ease: 'back.in(1.02)',
+  });
 };
