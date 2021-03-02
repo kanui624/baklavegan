381a480f7238f4b7066454e06cf1d7d0732833c3
Kanui <marley.kanui@gmail.com>
2021-03-01 23:52:31 -0600

    baklavegan - global commit 165 - Trying to fix react-spring bug in production 1.2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,25 @@
+// const withTM = require('next-transpile-modules')([
+//   'three',
+//   'react-spring',
+//   'postprocessing',
+//   '@react-three/drei',
+// ]);
+
+// module.exports = withTM();
 const withTM = require('next-transpile-modules')([
+  '@react-three/drei',
   'three',
   'react-spring',
-  'postprocessing',
-  '@react-three/drei',
+  'react-three-fiber',
 ]);
 
-module.exports = withTM();
+module.exports = withTM({
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /react-spring/,
+      sideEffects: true,
+    });
+
+    return config;
+  },
+});
